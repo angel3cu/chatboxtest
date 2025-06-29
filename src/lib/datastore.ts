1c9a202791c35d2308bdfa3110876b6a4844eac4
Angelos Sfakianakis <angel3cu@gmail.com>
2025-06-29 03:03:28 +0100

    Create a datastore to save user information.

--- src/lib/datastore.ts
+++ src/lib/datastore.ts
@@ -10,5 +10,6 @@ export default class Datastore {
   write(userId: string, key: string, value: string): void {
     const userData: Map<string, string> = this.data.get(userId) ?? new Map();
     userData.set(key, value);
+    this.data.set(userId, userData);
   }
 }
